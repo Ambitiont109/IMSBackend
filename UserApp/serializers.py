a29b610d88152825d46a4ac242119bda9424aa13
Ambition <ambition.t109@gmail.com>
2020-09-21 11:35:57 -0400

    Complete 60% USerApp

--- UserApp/serializers.py
+++ UserApp/serializers.py
@@ -3,17 +3,30 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.core.exceptions import ObjectDoesNotExist
 from .models import User
+import json
 
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=50, validators=[UniqueValidator(queryset=User.objects.all())])
+    classNames = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'picture', 'classnames')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'picture', 'classnames','classNames')
 
+    def get_classNames(self, obj):
+        try:
+            data = json.loads(obj.classnames)
+            if(type(data) == type([])):
+                return data
+            return []
+        except Exception as e:
+            return []
 
 class PasswordSerializer(serializers.Serializer):
     current_pwd = serializers.CharField(required=True)
     new_pwd = serializers.CharField(required=True)
-    model = User
+
+
+class PasswordSerializerTwo(serializers.Serializer):
+    new_pwd = serializers.CharField(required=True)
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, mixins, status
 from UserApp.models import User
-from UserApp.serializers import UserSerializer, PasswordSerializer
+from UserApp.serializers import UserSerializer, PasswordSerializer, PasswordSerializerTwo
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -28,6 +28,18 @@ class UserViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             if not user.check_password(serializer.data['current_pwd']):
                 return Response({"current_pwd":['Current Password is Incorrect']}, status = status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.data['new_pwd'])
+            user.save()
+            return Response({'status': 'password set'})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def set_password(self, request, pk=None):
+        user = self.get_object()
+        serializer = PasswordSerializerTwo(data=request.data)
+        if serializer.is_valid():
             user.set_password(serializer.data['new_pwd'])
             user.save()
             return Response({'status': 'password set'})
