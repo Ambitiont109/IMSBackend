3ffa0d9f56dfe660e954795db676c0782f5cb240
Ambition <ambition.t109@gmail.com>
2020-10-10 15:50:31 -0700

    Add IMSUser to admin

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -5,3 +5,11 @@ from .models import User
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+
+
+class IMSUser(User):
+    class Meta:
+        proxy = True
+
+
+admin.site.register(IMSUser)
