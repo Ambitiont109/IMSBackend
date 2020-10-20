5d078aceb65a70054ae7fe8caabfb2c16bb3802f
Ambition <ambition.t109@gmail.com>
2020-10-20 17:05:10 -0400

    update asgi.py

--- anam_backend_main/asgi.py
+++ anam_backend_main/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anam_backend_main.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anam_backend_main.settings")
+django.setup()
+application = get_default_application()
 
-application = get_asgi_application()
+# origin setting
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anam_backend_main.settings')
+# application = get_asgi_application()
